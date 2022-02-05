97c0186e04d4fdc1c7ef4a56a8a5af805f4cdf17
codemasterdev <skillz1029@gmail.com>
2022-02-05 11:17:17 -0500

    added responsiveness, need to seperate css files

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     return (
 
         <footer id="footer">
-            <p>&copy; Devon Webster 2022. All rights reserved.</p>
+            <p><small>&copy; Devon Webster 2022</small></p>
             <p>
                 <a href="#top">Go To Top</a>
             </p>
